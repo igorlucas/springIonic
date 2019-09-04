6e7ff43c1a4e3b2bd9f7240adb77eb7f317b288c
Igor Lucas <igorlucas.silva@bol.com.br>
2019-09-04 12:08:02 -0300

    tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -23,32 +23,38 @@ export class ProfilePage {
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email).subscribe(res => {
         this.cliente = res;
-      }, error=>{})
+      }, error => {
+        if (error.status == 403)
+          this.navCtrl.setRoot("HomePage");
+      })
+    } else {
+      this.navCtrl.setRoot("HomePage");
     }
   }
 
   getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.id)
-    .subscribe(response => {
-      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
-      this.blobToDataURL(response).then(dataUrl => {
-        //let str : string = dataUrl as string;
-        //this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
-      });
-    },
-    error => {
-      //this.profileImage = 'assets/imgs/avatar-blank.png';
-    });
+      .subscribe(response => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+        this.blobToDataURL(response).then(dataUrl => {
+          //let str : string = dataUrl as string;
+          //this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+        });
+      },
+        error => {
+
+          //this.profileImage = 'assets/imgs/avatar-blank.png';
+        });
   }
 
-    // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
-    blobToDataURL(blob) {
-      return new Promise((fulfill, reject) => {
-          let reader = new FileReader();
-          reader.onerror = reject;
-          reader.onload = (e) => fulfill(reader.result);
-          reader.readAsDataURL(blob);
-      })
-    }
+  // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+  blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fulfill(reader.result);
+      reader.readAsDataURL(blob);
+    })
+  }
 
 }
